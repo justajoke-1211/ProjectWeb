9a802c9e3142ea37d2eba58c0d1751f95d51e31d
justajoke-1211 <ngotanbinh848@gmail.com>
2022-11-11 19:36:55 +0700

    Update

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-  $(".Excercise").onClick(function () {
-    $(".test").hide();
+  $(".Excercise").click(function () {
+    $(".content").append(
+      '<svg width="200" height="100">' +
+      '<rect width="200" height="100" style="fill:rgb(255,255,255);stroke-width:10;stroke:rgb(0,0,0)" />' +
+      '<text x="50" y="50" fill="black" >' + "Hello " + '</text>' +
+      '</svg>'
+    );
   });
 });
